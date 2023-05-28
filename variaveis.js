d7d79cf45b9a402cfb9c5b7d9b8fd056634883b4
André Gustavo <andregustavo147@gmail.com>
2023-05-27 22:28:14 -0300

    Create variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,30 @@
+// VARIÁVEIS
+
+let idade = 26 // Criando a primeira variável
+
+console.log(idade)
+
+// Tipos de variáveis
+
+idade = 30                  // Number
+let numero = 30             // Number
+let altura = 1.77           // Number
+let nome = 'André'          // String (textos)
+let estudando = true        // Booleano (true ou false)
+
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+
+let outra;
+
+console.log(outra)
+
+let nula = null
+
+console.log(nula)
+
+var variavel = 30

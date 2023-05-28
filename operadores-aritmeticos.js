90cde48fa36fdaf2c011036fc7657116a8ae4cd8
André Gustavo <andregustavo147@gmail.com>
2023-05-27 23:00:37 -0300

    Create operadores-aritmeticos.js

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,30 @@
+// > Operadores Aritméticos
+
+let primeiroNumero = 20 // Camel Case
+let segundoNumero = 2
+
+// Adição:              +
+// Subtração:           -
+// Multiplicação:       *
+// Divisão:             /
+// Exponenciação:       **
+// Resto da divisão:    %
+
+const soma = primeiroNumero + segundoNumero
+
+console.log('Soma =', soma)
+console.log('Subtração =', primeiroNumero - segundoNumero)
+console.log('Multiplicação =', primeiroNumero * segundoNumero)
+console.log('Divisão =', primeiroNumero / segundoNumero)
+console.log('Exponenciação =', primeiroNumero ** segundoNumero)
+console.log('Resto da divisão =', primeiroNumero % 3)
+
+// Precedência de operadores
+
+console.log((3 + 7) * 3)
+console.log((10 + 7 + 8) / 3)
+
+//Blibioteca Math
+
+console.log(Math.PI)
+console.log(Math.sqrt(16))
